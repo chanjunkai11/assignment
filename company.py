868df4312c6d2a8bef3fc5978a16c21e425241ef
chanjunkai11 <143473505+chanjunkai11@users.noreply.github.com>
2023-09-21 03:19:44 +0800

    Update company.py

--- company.py
+++ company.py
@@ -262,7 +262,7 @@ def deleteJob():
     num = cursor.fetchone()
     file_name = "com-id-" + str(num[1]) + "_job_desc_file" + str(num[0]) + ".txt"
     s3 = boto3.resource('s3')
-    s3.Bucket(custombucket).delete_object(Key=file_name)
+    s3.Object(custombucket, file_name).delete()
     delete_sql = "DELETE FROM job_portal WHERE job_id = %s"
     cursor.execute(delete_sql, (id))
     db_conn.commit()
